6f7d2594aa003e05496a789b9a03d1e93a02a53d
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-12 14:38:45 +0100

    rendered data to the browser

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.use(express.json());
+
+
+
+
+
+//Create your endpoints/route handlers
+
+app.get("/api/rates/", async (req, res) => {
+    const response = await axios.get(`https://api.exchangeratesapi.io/latest`)
+
+    let base = req.query.base
+    let currencies = req.query.currency;
+
+    //covert from the BASE to CURRENCIES
+ 
+    //RET
+    let body = {
+        base,
+        date: response.data.date,
+        currencies,
+        rates: response.data.rates
+    }
+    return res.json({
+        "results": {
+            body
+        }
+    })
+});
+
+
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
